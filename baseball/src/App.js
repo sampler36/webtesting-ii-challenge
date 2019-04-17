1d98e7bc4da13cc275b62cae52e64cc0961f6aef
Talent Antonio <antonio.talenty@gmail.com>
2019-04-17 08:47:34 +0100

    Added tests and all in one

--- baseball/src/App.js
+++ baseball/src/App.js
@@ -5,15 +5,55 @@ class App extends Component {
   state = {
     greeting:'',
       balls:0,
-      strikes:0
+      strikes:0,
+      fouls:0,
+      hits:0
   }
+  strike = e => {
+    e.preventDefault();
+    let strikes = this.state.strikes + 1;
+      this.setState({
+        strikes: strikes
+      })
+  };
+
+  ball = e => {
+    e.preventDefault();
+    let balls = this.state.balls + 1;
+      this.setState({
+        balls: balls
+      })
+  };
+
+  foul = e => {
+    e.preventDefault();
+    let fouls = this.state.fouls + 1;
+    this.setState({
+      fouls: fouls
+    });
+  };
+
+  hit = e => {
+    e.preventDefault();
+    let hits = this.state.hits + 1;
+    this.setState({
+      hits: hits
+    });
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h2>Baseball Score Board </h2>
         </header>
-        <Display />
+        <Display
+            {...this.state}
+            strike = {this.strike}
+            ball = {this.ball}
+            foul = {this.foul}
+            hit = {this.hit}
+ 
+        />
       </div>
     );
   }
--- baseball/src/components/Display.js
+++ baseball/src/components/Display.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
 
 class Display extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-        balls:0,
-        strikes:0
-    }
-  }
+ 
   render() {
     return (
       <div>
-          <h3>No Scores</h3>
+        <h3>Counts</h3>
         <div>
+          {/* {Strike} */}
+          <div data-testid="Strike">{`${this.props.strikes} Strikes`}</div>
+          <button data-testid="Strike" onClick={this.props.strike}>
+            Strike
+          </button>
+           {/* {Ball} */}
+          <div data-testid="Ball">{`${this.props.balls} Balls`}</div>
+          <button data-testid="Ball" onClick={this.props.ball}>
+            Ball
+          </button>
 
+            {/* {Fouls} */}
+          <div data-testid="Foul">{`${this.props.fouls} Fouls`}</div>
+
+          <button data-testid="Foul" onClick={this.props.foul}>
+            Foul
+          </button>
+
+             {/* {Hits} */}
+          <div data-testid="Hit">{`${this.props.hits} Hit`}</div>
+          <button data-testid="Hit" onClick={this.props.hit}>
+            Hit
+          </button>
         </div>
       </div>
     );
